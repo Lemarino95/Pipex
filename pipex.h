8d9cab0cdfc01309beabb2cada06378451d9d708
Leonardo Marino <leo-marino@hotmail.it>
2025-04-01 20:40:41 +0200

    Add files via upload

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lemarino <lemarino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 16:33:20 by lemarino          #+#    #+#             */
-/*   Updated: 2025/03/30 19:49:40 by lemarino         ###   ########.fr       */
+/*   Updated: 2025/04/01 20:21:10 by lemarino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,35 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MACROS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 /*~~~~~~~~~~~~~~~COLORS~~~~~~~~~~~~~~~*/
-# define NO_COLOR "\033[0m"
+# define NO_ALL "\033[0m"
+
+# define NO_COLOR "\e[39m"
+# define BLACK "\e[30m"
 # define RED "\033[31m"
-# define GREEN "\033[32m"
+# define GREEN "\e[32m"
+# define BRGREEN "\033[32;1m"
 # define YELLOW "\033[33m"
 # define BLUE "\033[34m"
 # define MAGENTA "\033[95m"
-# define CYAN "\033[36m"
+# define BRCYAN "\033[96m"
+
+/*~~~~~~~~~~~~BACKGROUNDS~~~~~~~~~~~~~*/
+# define BGMAGENTA "\033[45m"
+# define BLACK_ON_WHITE "\e[107;30m"
+# define NO_BG "\e[49m"
+
+/*~~~~~~~~~~~~~~~MORE~~~~~~~~~~~~~~~*/
+# define BOLD "\e[1m"// "\e[1;4m" Bold and Underlined
+# define NO_BOLD "\e[21m"
+
+# define UNDERLINE "\e[4m"
+# define NO_UNDERLINE "\e[24m"
+
+# define BLINK "\e[5m"//Doesn't work on vscode terminal
+# define NO_BLINK "\e[25m"
+
+# define HIDE "\e[8m"
+# define NO_HIDE "\e[28m"
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
