60220d4a6dea948767796566c8e2ccadccba403a
Leonardo Marino <leo-marino@hotmail.it>
2025-04-05 17:03:26 +0200

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -6,67 +6,82 @@
 /*   By: lemarino <lemarino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 21:01:50 by lemarino          #+#    #+#             */
-/*   Updated: 2025/04/04 19:11:14 by lemarino         ###   ########.fr       */
+/*   Updated: 2025/04/05 17:02:18 by lemarino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	child_process(char *av[], char **envp, int *pipefd)
+void	cmd1_process(char *av[], char **envp, int *pipefd)
 {
 	int		infile;
 
 	close(pipefd[0]);
 	infile = open(av[1], O_RDONLY);
 	if (-1 == infile)
-		return(close(pipefd[1]), perror(RED"Invalid input file"), exit(0));
+		return(close(pipefd[1]), perror(RED"Invalid input file"), exit(2));
 	dup2(infile, STDIN_FILENO);
 	close(infile);
-	close(0);
 	dup2(pipefd[1], STDOUT_FILENO);
 	close(pipefd[1]);
-	close(1);
 	execute_cmd(av[2], envp, "First");
 }
 
-void	daddy_process(char *av[], char **envp, int *pipefd)
+void	cmd2_process(char *av[], char **envp, int *pipefd)
 {
 	int		outfile;
 
 	close(pipefd[1]);
 	outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (-1 == outfile)
-		return(close(pipefd[0]), perror(RED"Invalid output file"), exit(0));
+		return(close(pipefd[0]), perror(RED"Invalid output file"), exit(3));
 	dup2(pipefd[0], STDIN_FILENO);
 	close(pipefd[0]);
-	close(0);
 	dup2(outfile, STDOUT_FILENO);
 	close(outfile);
-	close(1);
 	execute_cmd(av[3], envp, "Second");
 }
+
+/* void	firstborn(char *av[], char **envp)
+{
+	int	pipefd[2];
+	int	id1;
+	int	id2;
+
+	if (-1 == pipe(pipefd))
+		return (perror(RED"Failed to create pipe"), exit(4));
+	id1 = fork();
+	if (0 == id1)
+		cmd1_process(av, envp, pipefd);
+	id2 = fork();
+	if (0 == id2)
+		cmd2_process(av, envp, pipefd);
+	close(pipefd[0]);
+	close(pipefd[1]);
+} */
 
 int	main(int ac, char *av[], char **envp)
 {
 	int	pipefd[2];
-	int	id;
+	int	id1;
 	int	id2;
 
 	if (5 != ac)
-		return (perror(RED"Invalid number of arguments"), 0);
+		return (perror(RED"Invalid number of arguments"), 1);
 	if (-1 == pipe(pipefd))
-		return (perror(RED"Failed to create pipe"), 0);
-	id = fork();
-	if (id < 0)
-	{
-		close(pipefd[0]);
-		close(pipefd[1]);
-		return (perror(RED"Fork failed"), 0);
-	}
-	if (0 == id)
-		child_process(av, envp, pipefd);
+		return (perror(RED"Failed to create pipe"), 1);
+	id1 = fork();
+	if (id1 < 0)
+		return (perror("Fork failed"), close(pipefd[0]), close(pipefd[1]), 1);
+	else if (0 == id1)
+		cmd1_process(av, envp, pipefd);
 	id2 = fork();
-	if (0== id2)
-		daddy_process(av, envp, pipefd);
-	wait(NULL);
+	if (id2 < 0)
+		return (perror("Fork failed"), close(pipefd[0]), close(pipefd[1]), 1);
+	else if (0 == id2)
+		cmd2_process(av, envp, pipefd);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	while(waitpid(-1, NULL, 0) > 0)
+		;
 }
